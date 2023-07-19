f599e1614ac418f0516276e58a81ff9868ee5b95
ocartaxo <oacartax@gmail.com>
2023-07-19 00:46:35 -0300

    cors

--- src/main/kotlin/br/com/ocartaxo/komilles/infra/security/SecurityConfigs.kt
+++ src/main/kotlin/br/com/ocartaxo/komilles/infra/security/SecurityConfigs.kt
@@ -2,9 +2,13 @@ package br.com.ocartaxo.komilles.infra.security
 
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.http.HttpMethod
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.web.DefaultSecurityFilterChain
+import org.springframework.web.cors.CorsConfiguration
+import org.springframework.web.cors.CorsConfigurationSource
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource
 import kotlin.jvm.Throws
 
 @Configuration
@@ -18,5 +22,23 @@ class SecurityConfigs {
         http.authorizeHttpRequests { requests -> requests.anyRequest().permitAll() }
 
         return http.build()
+    }
+
+    @Bean
+    fun corsConfiguration(): CorsConfigurationSource {
+
+        val corsConfig = CorsConfiguration()
+        corsConfig.setAllowedOriginPatterns(arrayListOf("http://localhost:3000"))
+        corsConfig.allowedMethods = arrayListOf(
+            "GET",
+            "POST",
+            "PUT",
+            "PATCH",
+            "DELETE"
+        )
+
+        val source = UrlBasedCorsConfigurationSource()
+        source.registerCorsConfiguration("/**", corsConfig)
+        return source
     }
 }
