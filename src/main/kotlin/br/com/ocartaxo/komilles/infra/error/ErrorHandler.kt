c3b27a39d003bb4fd8503f95c976917e858ec9a1
ocartaxo <oacartax@gmail.com>
2023-07-20 01:04:18 -0300

    error handler

--- src/main/kotlin/br/com/ocartaxo/komilles/infra/error/ErrorHandler.kt
+++ src/main/kotlin/br/com/ocartaxo/komilles/infra/error/ErrorHandler.kt
@@ -1,17 +1,24 @@
 package br.com.ocartaxo.komilles.infra.error
 
+import jakarta.servlet.http.HttpServletRequest
+import org.springframework.http.HttpStatus
+import org.springframework.http.HttpStatusCode
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.RestControllerAdvice
+import java.time.LocalDateTime
 
 @RestControllerAdvice
 class ErrorHandler {
 
     @ExceptionHandler(RuntimeException::class)
-    fun handleRuntimeException(ex: RuntimeException): ResponseEntity<Any> {
+    fun handleRuntimeException(ex: RuntimeException, request: HttpServletRequest): ResponseEntity<Any> {
         return ResponseEntity.internalServerError().body(object {
             val message = "Error não identificado!"
-            val errMsg = ex.localizedMessage
+            val error = ex.localizedMessage
+            val timestamp = LocalDateTime.now()
+            val statusCode = HttpStatus.INTERNAL_SERVER_ERROR
+            val path = request.requestURI
         })
     }
 
