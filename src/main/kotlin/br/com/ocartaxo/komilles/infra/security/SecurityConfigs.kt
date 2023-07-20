a92f3f640ea63e4bcfa7a8e55d7bfe42fd0995f1
ocartaxo <oacartax@gmail.com>
2023-07-20 01:04:27 -0300

    desabilita csrf

--- src/main/kotlin/br/com/ocartaxo/komilles/infra/security/SecurityConfigs.kt
+++ src/main/kotlin/br/com/ocartaxo/komilles/infra/security/SecurityConfigs.kt
@@ -2,14 +2,12 @@ package br.com.ocartaxo.komilles.infra.security
 
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.http.HttpMethod
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
-import org.springframework.security.web.DefaultSecurityFilterChain
+import org.springframework.security.web.SecurityFilterChain
 import org.springframework.web.cors.CorsConfiguration
 import org.springframework.web.cors.CorsConfigurationSource
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource
-import kotlin.jvm.Throws
 
 @Configuration
 @EnableWebSecurity
@@ -17,28 +15,12 @@ class SecurityConfigs {
 
     @Bean
     @Throws(Exception::class)
-    fun filterChain(http: HttpSecurity): DefaultSecurityFilterChain? {
+    fun filterChain(http: HttpSecurity): SecurityFilterChain {
 
-        http.authorizeHttpRequests { requests -> requests.anyRequest().permitAll() }
+        return http.authorizeHttpRequests { request ->
+            request.anyRequest().permitAll()
+        }.csrf { csrf -> csrf.disable() }.build()
 
-        return http.build()
     }
 
-    @Bean
-    fun corsConfiguration(): CorsConfigurationSource {
-
-        val corsConfig = CorsConfiguration()
-        corsConfig.setAllowedOriginPatterns(arrayListOf("http://localhost:3000"))
-        corsConfig.allowedMethods = arrayListOf(
-            "GET",
-            "POST",
-            "PUT",
-            "PATCH",
-            "DELETE"
-        )
-
-        val source = UrlBasedCorsConfigurationSource()
-        source.registerCorsConfiguration("/**", corsConfig)
-        return source
-    }
 }
