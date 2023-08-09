1c6787855b6de7434dd2dac297ccc5e93c64f3ae
ocartaxo <oacartax@gmail.com>
2023-08-09 20:27:57 -0300

    dependências

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,6 +5,7 @@ plugins {
 	id("io.spring.dependency-management") version "1.1.0"
 	id("org.graalvm.buildtools.native") version "0.9.23"
 	id("org.flywaydb.flyway") version "9.21.1"
+	id("io.ktor.plugin") version "2.3.3"
 	kotlin("jvm") version "1.8.22"
 	kotlin("plugin.spring") version "1.8.22"
 	kotlin("plugin.jpa") version "1.8.22"
@@ -37,6 +38,10 @@ dependencies {
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.flywaydb:flyway-core")
+	implementation("io.github.oshai:kotlin-logging-jvm:5.0.1")
+	implementation("com.aallam.openai:openai-client:3.3.0")
+	implementation("io.ktor:ktor-client-apache5:2.3.3")
+	implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core")
 	compileOnly("org.projectlombok:lombok")
 	developmentOnly("org.springframework.boot:spring-boot-devtools")
 	runtimeOnly("org.postgresql:postgresql")
@@ -46,6 +51,16 @@ dependencies {
 	testImplementation("org.springframework.security:spring-security-test")
 	testImplementation("io.mockk:mockk:1.13.5")
 }
+
+extra["springCloudVersion"] = "2022.0.4"
+
+dependencyManagement {
+	imports {
+		mavenBom("org.springframework.cloud:spring-cloud-dependencies:${property("springCloudVersion")}")
+	}
+}
+
+
 
 tasks.withType<KotlinCompile> {
 	kotlinOptions {
