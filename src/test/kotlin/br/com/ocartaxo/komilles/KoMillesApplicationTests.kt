1cc26418f202fa946a1dae5c9d2e33d99a08fd6c
ocartaxo <oacartax@gmail.com>
2023-08-09 21:19:40 -0300

    nomeclatura

--- src/test/kotlin/br/com/ocartaxo/komilles/KoMillesApplicationTests.kt
+++ src/test/kotlin/br/com/ocartaxo/komilles/KoMillesApplicationTests.kt
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
 
 @SpringBootTest
-class KomillesApplicationTests {
+class KoMillesApplicationTests {
 
 	@Test
 	fun contextLoads() {
