073ff62b2fb659ec68804e354952cee6dcad354e
ocartaxo <oacartax@gmail.com>
2023-08-14 03:11:08 -0300

    dependencia

--- build.gradle.kts
+++ build.gradle.kts
@@ -37,7 +37,6 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-security")
 	implementation("org.springframework.boot:spring-boot-starter-validation")
 	implementation("org.springframework.boot:spring-boot-starter-web")
-	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.flywaydb:flyway-core")
 	implementation("io.github.oshai:kotlin-logging-jvm:5.0.1")
