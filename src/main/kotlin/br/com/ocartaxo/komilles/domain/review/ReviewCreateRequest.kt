850f00b26520fe3ed802a13038d6be0183281eb3
ocartaxo <oacartax@gmail.com>
2023-08-14 02:42:32 -0300

    anotação & nome do campo

--- src/main/kotlin/br/com/ocartaxo/komilles/domain/review/ReviewCreateRequest.kt
+++ src/main/kotlin/br/com/ocartaxo/komilles/domain/review/ReviewCreateRequest.kt
@@ -1,19 +1,17 @@
 package br.com.ocartaxo.komilles.domain.review
 
 import com.fasterxml.jackson.annotation.JsonProperty
-import com.nimbusds.jose.shaded.gson.annotations.SerializedName
 import jakarta.validation.constraints.NotBlank
+import kotlinx.serialization.Serializable
 
-data class ReviewCreateRequest(
+@Serializable
+data class ReviewRequest(
     @NotBlank
     @JsonProperty("usuario")
-    @SerializedName("usuario")
     val username: String,
     @NotBlank
-    @JsonProperty("analise")
-    @SerializedName("analise")
-    val comment: String,
+    @JsonProperty("depoimento")
+    val review: String,
     @JsonProperty("foto")
-    @SerializedName("foto")
     val photo: String
 )
