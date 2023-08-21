59d4272262cec947462d2a19e87a06814169a9d7
ocartaxo <oacartax@gmail.com>
2023-08-21 09:41:16 -0300

    destination update

--- src/main/kotlin/br/com/ocartaxo/komilles/domain/destination/Destination.kt
+++ src/main/kotlin/br/com/ocartaxo/komilles/domain/destination/Destination.kt
@@ -3,28 +3,28 @@ package br.com.ocartaxo.komilles.domain.destination
 import jakarta.persistence.*
 
 @Entity
-@Table(name="destinos")
+@Table(name = "destinos")
 data class Destination(
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     val id: Int? = null,
-    @Column(name="nome")
+    @Column(name = "nome")
     val name: String,
-    @Column(name="preco")
+    @Column(name = "preco")
     var price: Double = 0.0,
-    @Column(name="foto1")
+    @Column(name = "foto1")
     var thumbnailPhoto: String,
-    @Column(name="foto2")
+    @Column(name = "descricao")
+    var description: String,
+    @Column(name = "foto2")
     var optionalPhoto: String? = null,
-    @Column(name="descricao")
-    var description: String? = null,
-    @Column(name="_meta")
+    @Column(name = "_meta")
     var meta: String? = null
 ) {
-    fun update(body: DestinationUpdateRequest){
-        this.thumbnailPhoto = body.thumbnailPhoto
+    fun update(body: DestinationUpdateRequest) {
+        this.thumbnailPhoto = body.thumbnailPhoto ?: this.thumbnailPhoto
         this.optionalPhoto = body.photo
-        this.price = body.price
-        this.description = body.description
+        this.price = body.price ?: this.price
+        this.description = body.description ?: this.description
         this.meta = body.meta
     }
 }
