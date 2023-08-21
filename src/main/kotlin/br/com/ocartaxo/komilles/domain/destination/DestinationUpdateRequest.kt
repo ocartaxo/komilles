259da91c022b01a1b968f062ca4b99f4346e3418
ocartaxo <oacartax@gmail.com>
2023-08-21 09:41:59 -0300

    destination update nullable fields

--- src/main/kotlin/br/com/ocartaxo/komilles/domain/destination/DestinationUpdateRequest.kt
+++ src/main/kotlin/br/com/ocartaxo/komilles/domain/destination/DestinationUpdateRequest.kt
@@ -5,13 +5,13 @@ import com.fasterxml.jackson.annotation.JsonProperty
 data class DestinationUpdateRequest(
     val id: Int,
     @JsonProperty("preco")
-    val price: Double,
+    val price: Double ? = null,
     @JsonProperty("foto1")
-    val thumbnailPhoto: String,
+    val thumbnailPhoto: String ? = null,
     @JsonProperty("foto2")
-    val photo: String,
+    val photo: String ? = null,
     @JsonProperty("meta")
-    val meta: String,
+    val meta: String ? = null,
     @JsonProperty("descricao")
     val description: String? = null
 )
